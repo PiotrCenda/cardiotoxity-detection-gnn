5d2001b2693f1e71266b6c34669caa8b0018d909
Piotr Cenda <piotr.cenda222@gmail.com>
2022-12-14 01:30:30 +0100

    added chebconv, dconv and dcrnn

--- grph_conv.py
+++ grph_conv.py
@@ -11,8 +11,9 @@ from torch_geometric.nn.conv import MessagePassing
 from torch_geometric.nn.dense.linear import Linear
 from torch_geometric.nn.inits import zeros
 from torch_geometric.typing import Adj, OptTensor
-from torch_geometric.utils import add_remaining_self_loops
+from torch_geometric.utils import add_remaining_self_loops, to_dense_adj, add_self_loops, get_laplacian, remove_self_loops, dense_to_sparse
 from torch_geometric.utils.num_nodes import maybe_num_nodes
+
 
 
 def gcn_norm(edge_index, edge_weight=None, num_nodes=None, improved=False,
@@ -56,25 +57,17 @@ def gcn_norm(edge_index, edge_weight=None, num_nodes=None, improved=False,
         return edge_index, deg_inv_sqrt[row] * edge_weight * deg_inv_sqrt[col]
 
 
-class OursGCNConv(MessagePassing):
+class GCNConv(MessagePassing):
     r"""The graph convolutional operator from the `"Semi-supervised
     Classification with Graph Convolutional Networks"
     <https://arxiv.org/abs/1609.02907>`_ paper
-    Shapes:
-        - **input:**
-          node features :math:`(|\mathcal{V}|, F_{in})`,
-          edge indices :math:`(2, |\mathcal{E}|)`,
-          edge weights :math:`(|\mathcal{E}|)` *(optional)*
-        - **output:** node features :math:`(|\mathcal{V}|, F_{out})`
     """
 
     _cached_edge_index: Optional[Tuple[Tensor, Tensor]]
     _cached_adj_t: Optional[SparseTensor]
 
-    def __init__(self, in_channels: int, out_channels: int,
-                 improved: bool = False, cached: bool = False,
-                 add_self_loops: bool = True, normalize: bool = True,
-                 bias: bool = True, **kwargs):
+    def __init__(self, in_channels: int, out_channels: int, improved: bool = False, cached: bool = False,
+                 add_self_loops: bool = True, normalize: bool = True, bias: bool = True, **kwargs):
 
         kwargs.setdefault('aggr', 'add')
         super().__init__(**kwargs)
@@ -89,8 +82,7 @@ class OursGCNConv(MessagePassing):
         self._cached_edge_index = None
         self._cached_adj_t = None
 
-        self.lin = Linear(in_channels, out_channels, bias=False,
-                          weight_initializer='glorot')
+        self.lin = Linear(in_channels, out_channels, bias=False, weight_initializer='glorot')
 
         if bias:
             self.bias = Parameter(torch.Tensor(out_channels))
@@ -105,10 +97,7 @@ class OursGCNConv(MessagePassing):
         self._cached_edge_index = None
         self._cached_adj_t = None
 
-    def forward(self, x: Tensor, edge_index: Adj,
-                edge_weight: OptTensor = None) -> Tensor:
-        """"""
-
+    def forward(self, x: Tensor, edge_index: Adj, edge_weight: OptTensor = None) -> Tensor:
         if self.normalize:
             if isinstance(edge_index, Tensor):
                 cache = self._cached_edge_index
@@ -147,3 +136,286 @@ class OursGCNConv(MessagePassing):
 
     def message_and_aggregate(self, adj_t: SparseTensor, x: Tensor) -> Tensor:
         return matmul(adj_t, x, reduce=self.aggr)
+    
+
+class ChebConv(MessagePassing):
+    r"""The chebyshev spectral graph convolutional operator from the
+    `"Convolutional Neural Networks on Graphs with Fast Localized Spectral
+    Filtering" <https://arxiv.org/abs/1606.09375>`_ paper
+    """
+    def __init__(self, in_channels: int, out_channels: int, K: int, normalization: Optional[str] = 'sym', bias: bool = True, **kwargs):
+        kwargs.setdefault('aggr', 'add')
+        super().__init__(**kwargs)
+
+        assert K > 0
+        assert normalization in [None, 'sym', 'rw'], 'Invalid normalization'
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.normalization = normalization
+        self.lins = torch.nn.ModuleList([
+            Linear(in_channels, out_channels, bias=False,
+                   weight_initializer='glorot') for _ in range(K)
+        ])
+
+        if bias:
+            self.bias = Parameter(Tensor(out_channels))
+        else:
+            self.register_parameter('bias', None)
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        for lin in self.lins:
+            lin.reset_parameters()
+        zeros(self.bias)
+
+    def __norm__(self, edge_index: Tensor, num_nodes: Optional[int], edge_weight: OptTensor, normalization: Optional[str], 
+                 lambda_max: OptTensor = None, dtype: Optional[int] = None, batch: OptTensor = None):
+        edge_index, edge_weight = remove_self_loops(edge_index, edge_weight)
+
+        edge_index, edge_weight = get_laplacian(edge_index, edge_weight,
+                                                normalization, dtype,
+                                                num_nodes)
+        assert edge_weight is not None
+
+        if lambda_max is None:
+            lambda_max = 2.0 * edge_weight.max()
+        elif not isinstance(lambda_max, Tensor):
+            lambda_max = torch.tensor(lambda_max, dtype=dtype,
+                                      device=edge_index.device)
+        assert lambda_max is not None
+
+        if batch is not None and lambda_max.numel() > 1:
+            lambda_max = lambda_max[batch[edge_index[0]]]
+
+        edge_weight = (2.0 * edge_weight) / lambda_max
+        edge_weight.masked_fill_(edge_weight == float('inf'), 0)
+
+        edge_index, edge_weight = add_self_loops(edge_index, edge_weight,
+                                                 fill_value=-1.,
+                                                 num_nodes=num_nodes)
+        assert edge_weight is not None
+
+        return edge_index, edge_weight
+
+    def forward(self, x: Tensor, edge_index: Tensor, edge_weight: OptTensor = None, batch: OptTensor = None, lambda_max: OptTensor = None) -> Tensor:
+        edge_index, norm = self.__norm__(edge_index, x.size(self.node_dim), edge_weight, self.normalization, lambda_max, dtype=x.dtype, batch=batch)
+
+        Tx_0 = x
+        Tx_1 = x 
+        out = self.lins[0](Tx_0)
+
+        if len(self.lins) > 1:
+            Tx_1 = self.propagate(edge_index, x=x, norm=norm, size=None)
+            out = out + self.lins[1](Tx_1)
+
+        for lin in self.lins[2:]:
+            Tx_2 = self.propagate(edge_index, x=Tx_1, norm=norm, size=None)
+            Tx_2 = 2. * Tx_2 - Tx_0
+            out = out + lin.forward(Tx_2)
+            Tx_0, Tx_1 = Tx_1, Tx_2
+
+        if self.bias is not None:
+            out = out + self.bias
+
+        return out
+
+    def message(self, x_j: Tensor, norm: Tensor) -> Tensor:
+        return norm.view(-1, 1) * x_j
+
+    def __repr__(self) -> str:
+        return (f'{self.__class__.__name__}({self.in_channels}, '
+                f'{self.out_channels}, K={len(self.lins)}, '
+                f'normalization={self.normalization})')
+
+
+class DConv(MessagePassing):
+    r"""An implementation of the Diffusion Convolution Layer.
+    For details see: `"Diffusion Convolutional Recurrent Neural Network:
+    Data-Driven Traffic Forecasting" <https://arxiv.org/abs/1707.01926>`
+    """
+
+    def __init__(self, in_channels, out_channels, K, bias=True):
+        super(DConv, self).__init__(aggr="add", flow="source_to_target")
+        assert K > 0
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.weight = torch.nn.Parameter(torch.Tensor(2, K, in_channels, out_channels))
+
+        if bias:
+            self.bias = torch.nn.Parameter(torch.Tensor(out_channels))
+        else:
+            self.register_parameter("bias", None)
+
+        self.__reset_parameters()
+
+    def __reset_parameters(self):
+        torch.nn.init.xavier_uniform_(self.weight)
+        torch.nn.init.zeros_(self.bias)
+
+    def message(self, x_j, norm):
+        return norm.view(-1, 1) * x_j
+
+    def forward(self, X: torch.FloatTensor, edge_index: torch.LongTensor, edge_weight: torch.FloatTensor) -> torch.FloatTensor:
+        r"""Making a forward pass. If edge weights are not present the forward pass
+        defaults to an unweighted graph.
+
+        Arg types:
+            * **X** (PyTorch Float Tensor) - Node features.
+            * **edge_index** (PyTorch Long Tensor) - Graph edge indices.
+            * **edge_weight** (PyTorch Long Tensor, optional) - Edge weight vector.
+
+        Return types:
+            * **H** (PyTorch Float Tensor) - Hidden state matrix for all nodes.
+        """
+        adj_mat = to_dense_adj(edge_index, edge_attr=edge_weight)
+        adj_mat = adj_mat.reshape(adj_mat.size(1), adj_mat.size(2))
+        deg_out = torch.matmul(adj_mat, torch.ones(size=(adj_mat.size(0), 1)).to(X.device))
+        deg_out = deg_out.flatten()
+        deg_in = torch.matmul(torch.ones(size=(1, adj_mat.size(0))).to(X.device), adj_mat)
+        deg_in = deg_in.flatten()
+
+        deg_out_inv = torch.reciprocal(deg_out)
+        deg_in_inv = torch.reciprocal(deg_in)
+        row, col = edge_index
+        norm_out = deg_out_inv[row]
+        norm_in = deg_in_inv[row]
+
+        reverse_edge_index = adj_mat.transpose(0, 1)
+        reverse_edge_index, vv = dense_to_sparse(reverse_edge_index)
+
+        Tx_0 = X
+        Tx_1 = X
+        H = torch.matmul(Tx_0, (self.weight[0])[0]) + torch.matmul(
+            Tx_0, (self.weight[1])[0]
+        )
+
+        if self.weight.size(1) > 1:
+            Tx_1_o = self.propagate(edge_index, x=X, norm=norm_out, size=None)
+            Tx_1_i = self.propagate(reverse_edge_index, x=X, norm=norm_in, size=None)
+            H = (
+                H
+                + torch.matmul(Tx_1_o, (self.weight[0])[1])
+                + torch.matmul(Tx_1_i, (self.weight[1])[1])
+            )
+
+        for k in range(2, self.weight.size(1)):
+            Tx_2_o = self.propagate(edge_index, x=Tx_1_o, norm=norm_out, size=None)
+            Tx_2_o = 2.0 * Tx_2_o - Tx_0
+            Tx_2_i = self.propagate(
+                reverse_edge_index, x=Tx_1_i, norm=norm_in, size=None
+            )
+            Tx_2_i = 2.0 * Tx_2_i - Tx_0
+            H = (
+                H
+                + torch.matmul(Tx_2_o, (self.weight[0])[k])
+                + torch.matmul(Tx_2_i, (self.weight[1])[k])
+            )
+            Tx_0, Tx_1_o, Tx_1_i = Tx_1, Tx_2_o, Tx_2_i
+
+        if self.bias is not None:
+            H += self.bias
+
+        return H
+
+
+class DCRNN(torch.nn.Module):
+    r"""An implementation of the Diffusion Convolutional Gated Recurrent Unit.
+    For details see: `"Diffusion Convolutional Recurrent Neural Network:
+    Data-Driven Traffic Forecasting" <https://arxiv.org/abs/1707.01926>`_
+
+    Args:
+        in_channels (int): Number of input features.
+        out_channels (int): Number of output features.
+        K (int): Filter size :math:`K`.
+        bias (bool, optional): If set to :obj:`False`, the layer
+            will not learn an additive bias (default :obj:`True`)
+    """
+
+    def __init__(self, in_channels: int, out_channels: int, K: int, bias: bool = True):
+        super(DCRNN, self).__init__()
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.K = K
+        self.bias = bias
+
+        self._create_parameters_and_layers()
+
+    def _create_update_gate_parameters_and_layers(self):
+        self.conv_x_z = DConv(
+            in_channels=self.in_channels + self.out_channels,
+            out_channels=self.out_channels,
+            K=self.K,
+            bias=self.bias,
+        )
+
+    def _create_reset_gate_parameters_and_layers(self):
+        self.conv_x_r = DConv(
+            in_channels=self.in_channels + self.out_channels,
+            out_channels=self.out_channels,
+            K=self.K,
+            bias=self.bias,
+        )
+
+    def _create_candidate_state_parameters_and_layers(self):
+        self.conv_x_h = DConv(
+            in_channels=self.in_channels + self.out_channels,
+            out_channels=self.out_channels,
+            K=self.K,
+            bias=self.bias,
+        )
+
+    def _create_parameters_and_layers(self):
+        self._create_update_gate_parameters_and_layers()
+        self._create_reset_gate_parameters_and_layers()
+        self._create_candidate_state_parameters_and_layers()
+
+    def _set_hidden_state(self, X, H):
+        if H is None:
+            H = torch.zeros(X.shape[0], self.out_channels).to(X.device)
+        return H
+
+    def _calculate_update_gate(self, X, edge_index, edge_weight, H):
+        Z = torch.cat([X, H], dim=1)
+        Z = self.conv_x_z(Z, edge_index, edge_weight)
+        Z = torch.sigmoid(Z)
+        return Z
+
+    def _calculate_reset_gate(self, X, edge_index, edge_weight, H):
+        R = torch.cat([X, H], dim=1)
+        R = self.conv_x_r(R, edge_index, edge_weight)
+        R = torch.sigmoid(R)
+        return R
+
+    def _calculate_candidate_state(self, X, edge_index, edge_weight, H, R):
+        H_tilde = torch.cat([X, H * R], dim=1)
+        H_tilde = self.conv_x_h(H_tilde, edge_index, edge_weight)
+        H_tilde = torch.tanh(H_tilde)
+        return H_tilde
+
+    def _calculate_hidden_state(self, Z, H, H_tilde):
+        H = Z * H + (1 - Z) * H_tilde
+        return H
+
+    def forward(self, X: torch.FloatTensor, edge_index: torch.LongTensor, edge_weight: torch.FloatTensor = None, H: torch.FloatTensor = None) -> torch.FloatTensor:
+        r"""Making a forward pass. If edge weights are not present the forward pass
+        defaults to an unweighted graph. If the hidden state matrix is not present
+        when the forward pass is called it is initialized with zeros.
+
+        Arg types:
+            * **X** (PyTorch Float Tensor) - Node features.
+            * **edge_index** (PyTorch Long Tensor) - Graph edge indices.
+            * **edge_weight** (PyTorch Long Tensor, optional) - Edge weight vector.
+            * **H** (PyTorch Float Tensor, optional) - Hidden state matrix for all nodes.
+
+        Return types:
+            * **H** (PyTorch Float Tensor) - Hidden state matrix for all nodes.
+        """
+        H = self._set_hidden_state(X, H)
+        Z = self._calculate_update_gate(X, edge_index, edge_weight, H)
+        R = self._calculate_reset_gate(X, edge_index, edge_weight, H)
+        H_tilde = self._calculate_candidate_state(X, edge_index, edge_weight, H, R)
+        H = self._calculate_hidden_state(Z, H, H_tilde)
+        return H
